70dc595d64d53665596cecc5159586855c4cefd4
YazidKurdi <50083367+YazidKurdi@users.noreply.github.com>
2019-11-06 14:54:58 +0200

    Add files via upload

--- categorical-graph/categorical function.R
+++ categorical-graph/categorical function.R
@@ -1,24 +1,30 @@
 library(tidyverse)
 library(drlib)
 
-categorical_graph_working <- function(dataframe,categorical_columns,max_categories = 10){
-  dataframe %>%
-  gather(category, value, categorical_columns) %>%
-  count(category, value) %>%
-  group_by(category) %>%
-  top_n(max_categories, value) %>%
-  ungroup() %>%
-  mutate(value = reorder_within(value, n, category)) %>%
-  ggplot(aes(value, n)) +
-  geom_col() +
-  facet_wrap(~ category, scales = "free_y") +
-  scale_x_reordered() +
-  coord_flip() +
-  labs(title = "Categorical predictors",
-       y = "Count")
+cat_graph <- function(dataframe,max_categories = 10){
   
-}
+    if(dataframe %>% map_lgl(~!is.factor(.))%>% all() == TRUE){
+      stop("dataframe contains no factor columns")}
+    else{dataframe %>%
+        gather(category, value, which(map_lgl(., is.factor))) %>%
+        count(category, value) %>%
+        group_by(category) %>%
+        top_n(max_categories, value) %>%
+        ungroup() %>%
+        mutate(value = reorder_within(value, n, category)) %>%
+        ggplot(aes(value, n)) +
+        geom_col() +
+        facet_wrap(~ category, scales = "free_y") +
+        scale_x_reordered() +
+        coord_flip() +
+        labs(title = "Categorical predictors",
+             y = "Count",
+             x = "")}
+  }
 
-categorical_graph(mtcars,categorical_columns = c("vs","am","mpg"),max_categories = 10)
+mtcars%>%
+  mutate_at(vars(vs,am,carb,gear),~as.factor(.))%>%
+  cat_graph()
+
 
 
